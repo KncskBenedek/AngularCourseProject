6116feb21bb62ff16e9dfb4ec9b34c94af1c3fbc
KncskBenedek <koncsikbenedek@gmail.com>
2022-12-01 16:07:19 +0100

    157, routolás kezd.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  melyik: string = 'recipies';
-
-  routeValt(event: string){
-    if(this.melyik != event.valueOf()){
-      this.melyik = event.valueOf();
-      
-      
-    }
     
     
-  }
+  
 }
