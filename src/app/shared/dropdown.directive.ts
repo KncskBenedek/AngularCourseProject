b28f7632a2bede013a3349a9692bf6c037ac843b
KncskBenedek <koncsikbenedek@gmail.com>
2022-12-04 12:25:49 +0100

    ?

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, OnInit } from '@angular/core';
+
+
+@Directive({
+    selector: '[appDropdown]'
+})
+
+export class DropdownDirective implements OnInit
+{
+
+    ngOnInit() {
+
+    }
+}
